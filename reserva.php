09a5c330710a6a0a69c1296edb04ea19c1c0f274
Gabriel Moreira Pimentel <gabriel.pimentel@academico.ifg.edu.br>
2023-09-21 19:20:53 -0300

    comeco reserva

---
+++ reserva.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Reserva</title>
+</head>
+<body>
+<nav class="container-fluid">
+    <div class="logo">Hotel do Gabes</div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col">Imagem 1</div>
+        <div class="col">Imagem 2</div>
+    </div>
+    <div class="row">
+        <div class="col">Imagem 3</div>
+        <div class="col">Imagem 4</div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="">
+                <h2>Titulo</h2>
+                <h3>Subtitulo</h3>
+                <hr>
+                <p>Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
+                    asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus necessitatibus
+                    nostrum quibusdam ratione rerum saepe ut velit. </p>
+                <p>Mostrar mais</p>
+            </div>
+            <hr>
+            <div class="">
+                <p>O que este lugar oferece</p>
+            </div>
+            <hr>
+            <div class="">
+                <p>Recursos</p>
+            </div>
+        </div>
+        <div class="col-md-6 d-flex justify-content-center">
+            <div class="border sticky-xxl-top card-flutuante">
+                <p>aaaa</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
