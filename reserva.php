8fc3210d46ac7a3c0db60af0d23124ae4813fc6d
Gabriel Moreira Pimentel <gabriel.pimentel@academico.ifg.edu.br>
2023-10-20 21:05:34 -0300

    commit

--- reserva.php
+++ reserva.php
@@ -21,18 +21,44 @@
 </header>
 <div class="container">
     <h1 class="acomodacao-title">Acomodação</h1>
-    <div class="d-flex justify-content-around">
-        <div class="box-img">
-
-        </div>
-        <div class="box-img">
-
-        </div>
-        <div class="box-img">
-
-        </div>
-        <div class="box-img">
-
+    <div class="imagens d-flex justify-content-around">
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
     <hr>
@@ -49,62 +75,34 @@
                         necessitatibus
                         nostrum quibusdam ratione rerum saepe ut velit.
 
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-                        animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-                        animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-                        animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-
-                        Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, aperiam
-                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
-                        necessitatibus
-                        nostrum quibusdam ratione rerum saepe ut velit.
-
                     </p>
                 </div>
-                <div class="mostrar-completo border rounded-4 p-4">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eaque illum ipsam laborum
-                        odit officia possimus quae quam quod tenetur? Delectus molestiae officia ratione! Asperiores
-                        eaque praesentium saepe sequi vero?
-                    </p>
+                <!-- Button trigger modal -->
+                <button type="button" class="mostrar-mais" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Mostrar Mais
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                     aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <p class="mostrar"><strong>Mostrar mais</strong></p>
-                <script>
-                    mostrar = document.querySelector(".mostrar");
-                    mostrar.onclick = function() {
-                        desc = document.querySelector(".mostrar-completo");
-                        desc.classList.toggle("ativo");
-                    }
-                </script>
+
             </div>
             <hr>
             <div class="">
@@ -117,22 +115,24 @@
         </div>
         <div class="col-md-6 d-flex justify-content-center">
             <div class="border rounded-4 p-4 sticky-xxl-top card-flutuante">
-                <div class="border rounded-3">
-                    <p>check-in</p>
-                    <p>check-out</p>
-                    <p>dados</p>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
                 </div>
-                <hr>
-                <div>
-                    <p>Valores</p>
-                    <p>Valores</p>
-                    <p>Valores</p>
-                    <p>Valor Final</p>
-                </div>
-                <button>Reservar</button>
             </div>
         </div>
     </div>
-</div>
 </body>
 </html>
