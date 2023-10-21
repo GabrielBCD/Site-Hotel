58434f1d6819b2085a59492e2b30082b75472a37
Gabriel Moreira Pimentel <gabriel.pimentel@academico.ifg.edu.br>
2023-10-20 22:21:36 -0300

    icones

--- reserva.php
+++ reserva.php
@@ -10,6 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="app/template/plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="assets/css/reserva.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <link rel="stylesheet" href="assets/css/nav.css">
@@ -20,49 +21,10 @@
     <div class="logo">Hotel do Gabe</div>
 </header>
 <div class="container">
-    <h1 class="acomodacao-title">Acomodação</h1>
-    <div class="imagens d-flex justify-content-around">
-        <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="assets/imagem/quarto.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+    <div class="box-img">
     </div>
     <hr>
-    <div class="row">
+    <div class="row justify-content-around">
         <div class="col-md-6">
             <div class="">
                 <h2 class="acomodacao-sub">Outro Nome</h2>
@@ -74,30 +36,41 @@
                         asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
                         necessitatibus
                         nostrum quibusdam ratione rerum saepe ut velit.
+                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
+                        necessitatibus
+                        nostrum quibusdam ratione rerum saepe ut velit.
+                        nostrum quibusdam ratione rerum saepe ut velit.
+                        asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
+                        necessitatibus
+                        nostrum quibusdam ratione rerum saepe ut velit.
 
                     </p>
                 </div>
-                <!-- Button trigger modal -->
-                <button type="button" class="mostrar-mais" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Mostrar Mais
-                </button>
 
+                <p data-bs-target="#exampleModal" data-bs-toggle="modal" class="mostrar-mais">Mostrar mais</p>
                 <!-- Modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                      aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Sobre essa merda</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                             </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                            <div class="modal-body text-modal">
+                                Descrição - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                                animi, aperiam
+                                asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
+                                necessitatibus
+                                nostrum quibusdam ratione rerum saepe ut velit.
+                                asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
+                                necessitatibus
+                                nostrum quibusdam ratione rerum saepe ut velit.
+                                nostrum quibusdam ratione rerum saepe ut velit.
+                                asperiores corporis culpa cupiditate doloribus ducimus est eveniet ipsam magni minus
+                                necessitatibus
+                                nostrum quibusdam ratione rerum saepe ut velit.
                             </div>
                         </div>
                     </div>
@@ -107,29 +80,31 @@
             <hr>
             <div class="">
                 <p>O que este lugar oferece</p>
+                <div>
+                    <i class="fas fa-wifi fa-lg"></i>
+                    <i class="fas fa-car fa-lg"></i>
+                </div>
             </div>
             <hr>
             <div class="">
                 <p>Recursos</p>
             </div>
         </div>
-        <div class="col-md-6 d-flex justify-content-center">
-            <div class="border rounded-4 p-4 sticky-xxl-top card-flutuante">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
+        <div class="col-md-4 d-flex justify-content-center">
+            <div class="card p-4">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
                 </div>
             </div>
         </div>
